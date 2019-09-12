81df9b1c16badfc88b48d47a38e006990f41c220
EletronSz <janetuker2@gmail.com>
2019-09-12 11:57:30 +0200

    edited .env

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
+
 const Mongoose = require("mongoose");
 
-let DB_URL="mongodb://165.22.221.50/Api"
-Mongoose.connect(DB_URL,{ useNewUrlParser: true, useUnifiedTopology: true  });
+Mongoose.connect(process.env.DB_URL,{ useNewUrlParser: true, useUnifiedTopology: true  });
 
 var db = Mongoose.connection;
 
