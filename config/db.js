7e1eb23b7fbcd1e76d45861da6538f9ffaf8fc4e
EletronSz <janetuker2@gmail.com>
2019-09-14 17:13:33 +0200

    Awesome

--- config/db.js
+++ config/db.js
@@ -2,8 +2,18 @@ require('dotenv').config()
 
 const Mongoose = require("mongoose");
 
-Mongoose.connect(process.env.DB_URL,{ useNewUrlParser: true, useUnifiedTopology: true  });
+const options = {
+    user: process.env.DB_USER,
+    pass: process.env.DB_PASS,
+    keepAlive: process.env.KEEP_ALIVE,
+    keepAliveInitialDelay: process.env.KEEP_ALIVE_DELAY,
+    useNewUrlParser: process.env.NEW_URL_PARSER,
+    useUnifiedTopology: process.env.USE_UNIFIED_TOPOLOGY
+};
+//connect to mongodb online
+Mongoose.connect(process.env.DB_URL_REMOTE,options);
 
 var db = Mongoose.connection;
 
+//export db for global state
 module.exports = db
