856c6069cd4a01940b5bce951a1ceb7355ca2058
ElectronSz <janetuker2@gmail.com>
2020-01-22 16:52:10 +0200

    maximum

--- express/index.js
+++ express/index.js
@@ -1,6 +1,7 @@
 const Express = require("express");
 const BodyParser = require("body-parser");
-const userRoutes = require('../routes/userRoute.js')
+const userRoutes = require('../routes/userRoute')
+const baseRoutes = require('../routes/baseRoutes')
 //const authRoutes = require('../routes/authRoutes')
 var cors = require('cors');
 //const authenticate = require('../middleware/authenticate');
@@ -13,6 +14,7 @@ app.use(BodyParser.urlencoded({ extended: true }));
 
 //register user routes
 app.use('/v1/user',userRoutes)
+app.use('/register', baseRoutes)
 // app.use('/v1/auth', authenticate, authRoutes)
 
 //export entire app
